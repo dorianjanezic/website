ccf2ce331ce086cde2de1d978492699eabee40be
Dorian Janezic <dtj5179@nyu.edu>
2021-10-11 18:18:56 +0200

    11_10

---
+++ public/projects/script5.js
@@ -0,0 +1,81 @@
+
+let theta = 0;
+
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    canvas.position(0,0);
+    canvas.style('z-index', '-1');
+  img = loadImage('wt-2.png');
+  img1 = loadImage('Scr.png');
+  img2 = loadImage('untld.png');
+  img3 = loadImage('lasers.png');
+ 
+}
+
+function draw() {
+  let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
+
+  
+
+  
+orbitControl(1,1,1);
+  background(0);
+  ambientLight(1500);
+  
+ 
+  
+
+push();
+
+  
+  translate(width/3,-width/8, -200)
+  rotateX(frameCount * 0.002)
+  rotateY(frameCount * 0.002);
+  texture(img);
+  box(width/5 ,width/5,width/5);
+pop(); theta += 0.03;
+
+
+push();
+  
+ 
+ translate(-width/3,width/8,-150)
+  rotateY(frameCount * -0.002)
+  rotateX(frameCount * -0.001)
+
+  rotateY(705)
+  texture(img2)
+  box(width/4,width/4, width/4)
+  pop();
+
+
+  push();
+  
+ 
+ translate(-width/20,-width/8,00)
+  rotateY(frameCount * -0.002)
+  rotateX(frameCount * 0.001)
+
+  rotateY(705)
+  texture(img1)
+  box((width/7 ,width/7,width/7))
+
+  pop();
+
+  push();
+
+  
+  translate(width/6,width/8, -100)
+  rotateX(frameCount * -0.002)
+  rotateY(frameCount * -0.002);
+  texture(img3);
+  box(width/8 ,width/8,width/8);
+pop();
+
+}
+
+function windowResized () {
+  resizeCanvas(windowWidth, windowHeight);
+}
